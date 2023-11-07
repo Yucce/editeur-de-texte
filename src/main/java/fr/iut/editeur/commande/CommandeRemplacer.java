3319558903c3e5eac1baa5710e4fe8749fa8175e
cabot <thomas.cabot@etu.umontpellier.fr>
2023-11-07 17:04:36 +0100

    ajout de la fonction remplacer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,23 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument{
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if (parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;debut;fin;texte");
+            return;
+        }
+        String texte = parameters[3];
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+
+        this.document.remplacer(debut,fin,texte);
+        super.executer();
+    }
+}
