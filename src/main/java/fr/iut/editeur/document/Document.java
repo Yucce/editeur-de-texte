1821915c7f001704a05f31096e2cd8ef486d060b
cabot <thomas.cabot@etu.umontpellier.fr>
2023-11-14 16:53:14 +0100

    feat : ajout de la javadoc.

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -1,5 +1,8 @@
 package fr.iut.editeur.document;
 
+import fr.iut.editeur.commande.CommandeAjouter;
+import fr.iut.editeur.commande.CommandeRemplacer;
+
 public class Document {
 
     private String texte;
@@ -16,10 +19,22 @@ public class Document {
         this.texte = texte;
     }
 
+    /**
+     * ajouter le texte passé en paramètre au document.
+     * Il peut être intéressant de consulter la méthode {@link CommandeAjouter#executer()} !
+     * @param texte String à ajouter au document
+     */
     public void ajouter(String texte) {
         this.texte += texte;
     }
 
+    /**
+     * remplace le texte de la case start a la case end par le texte passé en paramètre.
+     * Il peut être intéressant de consulter la méthode {@link CommandeRemplacer#executer()} !
+     * @param start indice du début du texte a remplacer
+     * @param end indice de la fin du texte a remplacer
+     * @param remplacement String à ajouter au document
+     */
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
